fd55479d2805451b8600e363aa3cdb7b1b546b0f
lorenzo1507 <lorenzocanelhas@gmail.com>
2022-05-06 22:12:40 -0300

    Aula de NoteActivities completa

---
+++ app/src/main/java/br/senac/noteapp/NoteSingleton.kt
@@ -0,0 +1,7 @@
+package br.senac.noteapp
+
+import br.senac.Note
+
+object NoteSingleton {
+    val noteList = arrayListOf<Note>()
+}
--- app/src/main/java/br/senac/noteapp/activities/ListNotesActivity.kt
+++ app/src/main/java/br/senac/noteapp/activities/ListNotesActivity.kt
@@ -1,8 +1,11 @@
 package br.senac.noteapp.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.senac.noteapp.NoteSingleton
 import br.senac.noteapp.databinding.ActivityListNotesBinding
+import br.senac.noteapp.databinding.NoteBinding
 
 class ListNotesActivity : AppCompatActivity() {
     lateinit var binding: ActivityListNotesBinding
@@ -12,6 +15,30 @@ class ListNotesActivity : AppCompatActivity() {
         binding = ActivityListNotesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.fab.setOnClickListener{
+            val intent = Intent(this, NewNoteActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        refreshNotes()
+    }
+    fun refreshNotes() {
+        binding.container.removeAllViews()
+
+        //for (item in NoteSingleton.noteList) ou...
+        NoteSingleton.noteList.forEach {
+            val cardBinding = NoteBinding.inflate(layoutInflater)
+
+            cardBinding.textTitle.text = it.title
+            cardBinding.textDesc.text = it.desc
+
+            binding.container.addView(cardBinding.root)
+        }
     }
 
 }
